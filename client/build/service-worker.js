58bb83454eaf515cf4069c5cb3614183686f4d67
Ali Kucukavci <ali91kavci@outlook.dk>
2022-03-20 18:00:10 +0100

    test

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8964d93cda1aa2c02c35a159b8c2a3d8.js"
+  "/precache-manifest.70dac3e3ea6d31bd19427f1da7ccc234.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
